c1ff5a198ecc4f8858f06a7050f81b058ac59855
NaoCraftLab <toxic.swoops_0j@icloud.com>
2024-10-04 03:46:44 +0400

    Release 1.1.0

---
+++ README.md
@@ -0,0 +1,47 @@
+<img src="src/main/resources/assets/foggy-pale-garden/icon.png" width="128">
+
+# Foggy Pale Garden
+
+---
+A Minecraft mod that adds thick fog to the Pale Garden biome.
+
+---
+
+## ✨ Features
+
+<details>
+  <summary>😱 Envelops the Pale Garden in fog</summary>
+
+![immersion-in-thick-fog.gif](docs/images/immersion-in-thick-fog.gif)
+
+</details>
+
+<details>
+  <summary>🕳️ The fog does not fill caves beneath the Pale Garden</summary>
+
+![no-fog-in-caves.gif](docs/images/no-fog-in-caves.gif)
+
+</details>
+
+<details>
+  <summary>🪽The fog doesn’t hinder flying over the Pale Garden</summary>
+
+![fog-doesn’t-hinder-flying.gif](docs/images/fog-doesn%E2%80%99t-hinder-flying.gif)
+
+</details>
+
+## 📥 Installation
+
+1. Install Minecraft version `24w40a` or newer
+2. If it's a snapshot, enable the Winter Drop experiment when creating a world
+3. Install [Fabric](https://fabricmc.net) or [Quilt](https://quiltmc.org) for this version
+4. Place the mod in the `mods` folder
+5. Beware of the monsters in the fog!
+
+## 🚀 Plans
+
+- [x] Add fog to the Pale Garden
+- [ ] Add configurations
+- [ ] Port to previous game versions and add support for mods backporting the Pale Garden
+- [ ] (After the Winter Drop release) Port to NeoForge
+- [ ] (After the Winter Drop release) Add visual configuration
--- src/main/java/com/naocraftlab/foggypalegarden/mixin/PaleGardenFogMixin.java
+++ src/main/java/com/naocraftlab/foggypalegarden/mixin/PaleGardenFogMixin.java
@@ -1,12 +1,14 @@
 package com.naocraftlab.foggypalegarden.mixin;
 
 import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.BackgroundRenderer.FogType;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.Fog;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.RaycastContext;
 import net.minecraft.world.biome.Biome;
 import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,20 +16,30 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Set;
+
 import static net.minecraft.client.render.FogShape.SPHERE;
+import static net.minecraft.world.LightType.SKY;
+import static net.minecraft.world.RaycastContext.FluidHandling.NONE;
+import static net.minecraft.world.RaycastContext.ShapeType.COLLIDER;
 
 @Mixin(BackgroundRenderer.class)
 public abstract class PaleGardenFogMixin {
 
-    private static final Identifier PALE_GARDEN_BIOME_ID = Identifier.of("minecraft", "pale_garden");
-    private static final float TRANSITION_SPEED = 0.006f;   // (0.0 to 1.0)
+    private static final Set<String> PALE_GARDEN_BIOME_IDS = Set.of(
+            "minecraft:pale_garden"
+    );
+    private static final float FOG_START = 0.0f;
+    private static final float FOG_END = 10.0f;
+    private static final float TRANSITION_SPEED = 0.003f;   // (0.0 to 1.0)
+    private static final int MIN_FLIGHT_HEIGHT = 15;
 
     private static float fogDensity = 0.0f;
 
     @Inject(method = "applyFog", at = @At("HEAD"), cancellable = true)
     private static void injectApplyFog(
             Camera camera,
-            BackgroundRenderer.FogType fogType,
+            FogType fogType,
             Vector4f color,
             float viewDistance,
             boolean thickenFog,
@@ -35,20 +47,26 @@ public abstract class PaleGardenFogMixin {
             CallbackInfoReturnable<Fog> cir
     ) {
         final ClientWorld world = (ClientWorld) camera.getFocusedEntity().getWorld();
-        final BlockPos cameraPos = camera.getBlockPos();
-        final RegistryEntry<Biome> biomeEntry = world.getBiome(cameraPos);
-        boolean currentlyInPaleGarden = biomeEntry.matchesId(PALE_GARDEN_BIOME_ID);
-        if (currentlyInPaleGarden) {
+        final BlockPos blockPos = camera.getBlockPos();
+        final RegistryEntry<Biome> biomeEntry = world.getBiome(blockPos);
+
+        final BlockHitResult hitResult = world.raycast(new RaycastContext(
+                blockPos.toCenterPos(), blockPos.add(0, -256, 0).toCenterPos(), COLLIDER, NONE, camera.getFocusedEntity()
+        ));
+        final double relativeHeight = blockPos.getY() - hitResult.getPos().y;
+
+        final boolean playerFlying = relativeHeight > MIN_FLIGHT_HEIGHT;
+        final boolean isSupportedBiome = PALE_GARDEN_BIOME_IDS.contains(biomeEntry.getIdAsString());
+        final boolean isNotCave = world.getLightLevel(SKY, blockPos) > 4;
+        if (isSupportedBiome && isNotCave && !playerFlying) {
             fogDensity = Math.min(fogDensity + TRANSITION_SPEED, 1.0f);
         } else {
             fogDensity = Math.max(fogDensity - TRANSITION_SPEED, 0.0f);
         }
 
         if (fogDensity > 0.0f) {
-            float fogStart = 0.0f;
-            float fogEnd = (1.0f - fogDensity) * viewDistance + fogDensity * 10.0f;
             final float brightness = (0.299f * color.x + 0.587f * color.y + 0.114f * color.z) * color.w;
-            final Fog fog = new Fog(fogStart, fogEnd, SPHERE, brightness, brightness, brightness, 1f);
+            final Fog fog = new Fog(FOG_START, FOG_END, SPHERE, brightness, brightness, brightness, fogDensity);
             cir.setReturnValue(fog);
             cir.cancel();
         }
