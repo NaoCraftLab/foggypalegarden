884a22e84e5fae72ceb87745d91bedd8740bf366
NaoCraftLab <toxic.swoops_0j@icloud.com>
2024-10-31 16:28:55 +0400

    Fix main

--- src/main/java/com/naocraftlab/foggypalegarden/FoggyPaleGardenClientMod.java
+++ src/main/java/com/naocraftlab/foggypalegarden/FoggyPaleGardenClientMod.java
@@ -16,19 +16,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Paths;
 
-// FIXME v2.8.0
-// FIXME поддержка модменю
-// FIXME перевод новых строк
-// FIXME чейнджлог
-// FIXME скрин
-// FIXME ридми
-// FIXME сменить modId и конввертировать старые конфиги
-// FIXME обновить описание и скрины на площадках
-
-// FIXME vNEXT
-// FIXME баг с яркостью неба при установки фиксированного цвета
-// FIXME сборка под неофорж
-// FIXME бекпорт на предыдущие версии игры???
 public class FoggyPaleGardenClientMod implements ClientModInitializer {
 
     public static final String MOD_ID = "foggypalegarden";
