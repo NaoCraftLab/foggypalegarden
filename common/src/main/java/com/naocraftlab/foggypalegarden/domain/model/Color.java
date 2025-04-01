2a11fd409d56064b65a4227c6c4e0e79c10e1157
NaoCraftLab <toxic.swoops_0j@icloud.com>
2025-04-01 05:10:31 +0400

    Refactor records to classes and add new Minecraft version support

--- common/src/main/java/com/naocraftlab/foggypalegarden/domain/model/Color.java
+++ common/src/main/java/com/naocraftlab/foggypalegarden/domain/model/Color.java
@@ -1,12 +1,30 @@
 package com.naocraftlab.foggypalegarden.domain.model;
 
 import lombok.Builder;
+import lombok.Data;
 
+@Data
 @Builder
-public record Color(
-        float red,
-        float green,
-        float blue,
-        float alpha
-) {
+public final class Color {
+
+    private final float red;
+    private final float green;
+    private final float blue;
+    private final float alpha;
+
+    public float red() {
+        return red;
+    }
+
+    public float green() {
+        return green;
+    }
+
+    public float blue() {
+        return blue;
+    }
+
+    public float alpha() {
+        return alpha;
+    }
 }
--- common/src/main/java/com/naocraftlab/foggypalegarden/domain/model/Environment.java
+++ common/src/main/java/com/naocraftlab/foggypalegarden/domain/model/Environment.java
@@ -1,20 +1,66 @@
 package com.naocraftlab.foggypalegarden.domain.model;
 
 import lombok.Builder;
+import lombok.Data;
 import net.minecraft.world.Difficulty;
 
+@Data
 @Builder
-public record Environment(
-        String dimension,
-        String biome,
-        Float biomeTemperature,
-        Difficulty difficulty,
-        Weather weather,
-        long timeOfDay,
-        int skyLightLevel,
-        double height,
-        double heightAboveSurface,
-        Color gameFogColor,
-        float fogDensity
-) {
+public final class Environment {
+
+    private final String dimension;
+    private final String biome;
+    private final Float biomeTemperature;
+    private final Difficulty difficulty;
+    private final Weather weather;
+    private final long timeOfDay;
+    private final int skyLightLevel;
+    private final double height;
+    private final double heightAboveSurface;
+    private final Color gameFogColor;
+    private final float fogDensity;
+
+    public String dimension() {
+        return dimension;
+    }
+
+    public String biome() {
+        return biome;
+    }
+
+    public Float biomeTemperature() {
+        return biomeTemperature;
+    }
+
+    public Difficulty difficulty() {
+        return difficulty;
+    }
+
+    public Weather weather() {
+        return weather;
+    }
+
+    public long timeOfDay() {
+        return timeOfDay;
+    }
+
+    public int skyLightLevel() {
+        return skyLightLevel;
+    }
+
+    public double height() {
+        return height;
+    }
+
+    public double heightAboveSurface() {
+        return heightAboveSurface;
+    }
+
+    public Color gameFogColor() {
+        return gameFogColor;
+    }
+
+    public float fogDensity() {
+        return fogDensity;
+    }
 }
